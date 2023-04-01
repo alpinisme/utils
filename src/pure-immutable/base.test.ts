9eace34c77bf17bbc60a71c7cd5bbdc1dca714c5
Matthew Nelson <menelso2@illinois.edu>
2023-04-01 13:46:54 -0500

    feat: add isNull and isNonNull

--- src/pure-immutable/base.test.ts
+++ src/pure-immutable/base.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from 'vitest'
-import { isError } from './base'
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { isError, isNonNull } from './base'
 
 describe('error', () => {
   test('isError identifies errors correctly', () => {
@@ -9,5 +9,23 @@ describe('error', () => {
     } catch (e) {
       expect(isError(e)).toBe(false)
     }
+  })
+})
+
+
+describe('null checks', () => {
+  test('filtering an array by isNonNull should result in an array typed as non-null values', () => {
+    let stringsAndNulls = ['oh', null, 'hey']
+    let strings = stringsAndNulls.filter(isNonNull)
+    expectTypeOf(strings).toMatchTypeOf(['key'])
+    expectTypeOf(strings).not.toMatchTypeOf([null])
+    expectTypeOf(stringsAndNulls).not.toMatchTypeOf(strings)
+  })
+
+  test('filtering an array by isNonNull should result in an array stripped of null', () => {
+    let stringsAndNulls = ['oh', null, 'hey', undefined]
+    let strings = stringsAndNulls.filter(isNonNull)
+    expect(strings).not.toEqual(stringsAndNulls)
+    expect(strings).toEqual(['oh', 'hey'])
   })
 })
--- src/pure-immutable/base.ts
+++ src/pure-immutable/base.ts
@@ -9,3 +9,19 @@ export function isError(err: unknown): err is Error {
 export function toString<T extends { toString: () => string }>(val: T): string {
   return val.toString()
 }
+
+/**
+ * type guard to narrow out null and undefined values -- useful for array filtering
+ */
+export const isNonNull = <T>(val: T | undefined | null): val is T => {
+  return val != null
+}
+
+/**
+ * type guard to check if null or undefined
+ */
+export const isNull = <T>(
+  val: T | undefined | null
+): val is undefined | null => {
+  return val == null
+}
