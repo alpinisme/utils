c7f6467608542cdd8538f525450aeb01d50146ac
Matthew Nelson <menelso2@illinois.edu>
2023-04-01 14:00:30 -0500

    feat: add has

--- src/pure-immutable/object.test.ts
+++ src/pure-immutable/object.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from 'vitest'
-import { isMatch, omit, pick } from './object'
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { has, isMatch, omit, pick } from './object'
 
 describe('picking and omitting', () => {
   test('pick includes specified keys and excludes others', () => {
@@ -26,5 +26,69 @@ describe('match testing', () => {
 
   test('isMatch rejects nonmatches', () => {
     expect(isMatch({ yes: 'no', huh: 'cool' }, { yes: 'yes' })).toBe(false)
+  })
+})
+
+describe('property checking', () => {
+  type Union = { max?: number } | { min: number; max: number } | { other: '' }
+
+  test('types of `has` when given a union where at least one member has key', () => {
+    const obj = {} as Union
+
+    if (has('max', obj)) {
+      expectTypeOf(obj.max).toBeNumber()
+    } else {
+      // @ts-expect-error obj.max is not known to exist
+      assertType(obj.max)
+    }
+  })
+
+  test('types of `has` when given a nullable union where at least one member has key', () => {
+    const obj = {} as Union | null
+
+    if (has('max', obj)) {
+      expectTypeOf(obj.max).toBeNumber()
+    } else {
+      // @ts-expect-error obj.max is not known to exist
+      assertType(obj.max)
+    }
+  })
+
+  test('types of `has` when given a union where no member has key', () => {
+    const obj = {} as Union
+
+    // @ts-expect-error obj.random is not known to exist
+    has('random', obj)
+  })
+
+  test('types of `has` when given a nullable union where no member has key', () => {
+    const obj = {} as Union | undefined
+
+    // @ts-expect-error obj.random is not known to exist
+    has('random', obj)
+  })
+
+  test('`has` fails if object lacks specified key', () => {
+    const obj = { other: '' } as Union
+
+    expect(has('max', obj)).toBe(false)
+  })
+
+  test('`has` fails if object has key but it is undefined or null', () => {
+    const obj = { max: null } as any
+
+    expect(has('max', obj)).toBe(false)
+  })
+
+  test('`has` fails if object has key but it is undefined or null', () => {
+    const obj = { max: undefined } as any
+
+    expect(has('max', obj)).toBe(false)
+  })
+
+  test('`has` passes if object has non-null key', () => {
+    const obj = { max: 19 } as Union
+
+    expect(has('max', obj)).toBe(true)
   })
 })
--- src/pure-immutable/object.ts
+++ src/pure-immutable/object.ts
@@ -1,4 +1,4 @@
-import { toString } from './base'
+import { isNull, toString } from './base'
 
 const { entries, fromEntries } = Object
 
@@ -34,3 +34,52 @@ export function omit<T extends Record<string, any>, Keys extends (keyof T)[]>(
     Keys[number]
   >
 }
+
+/**
+ *  extracts all keys that are guaranteed to be in T (i.e., if union type, only shared keys)
+ */
+export type Keys<T> = keyof T
+
+/**
+ * extracts all available keys from a union type, not just shared keys
+ */
+export type AllKeys<T> = T extends unknown ? Keys<T> : never
+
+/**
+ * turn optional field into required
+ */
+export type RequireKey<Obj, Key extends keyof Obj> = Obj & {
+  [P in Key]-?: Obj[P]
+}
+
+/**
+ *  filters union types to ones that have the specified key
+ */
+type SubtypesWithKey<Obj, Key extends PropertyKey> = Obj extends unknown
+  ? Key extends keyof Obj
+    ? Obj
+    : never
+  : never
+
+type Nullable<T> = T | null | undefined
+/**
+ * check if obj has a key with a non-null value
+ */
+export function has<Key extends AllKeys<T>, T extends {}>(
+  key: Key,
+  obj: Nullable<T>,
+): obj is RequireKey<SubtypesWithKey<T, Key>, Key> {
+  if (isNull(obj)) {
+    return false
+  }
+  return key in obj && obj[key] != null
+}
+
+/**
+ * curried version of `has`
+ */
+export const curriedHas =
+  <Key extends AllKeys<T>, T extends {}>(key: Key) =>
+  (obj: Nullable<T>): obj is RequireKey<SubtypesWithKey<T, Key>, Key> => {
+    return has(key, obj)
+  }
