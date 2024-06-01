b18b8452cc1872176b4357b5b12be9065cca7907
Matthew Nelson <menelso2@illinois.edu>
2024-06-01 10:51:03 -0500

    feat: batchWithBisectingRetries

--- src/pure-immutable/function.test.ts
+++ src/pure-immutable/function.test.ts
@@ -1,6 +1,12 @@
 import { describe, expect, test } from 'vitest'
 import { identity } from './base'
-import { flipArgs, safeInvoke, wrap } from './function'
+import {
+  asyncBatchWithBisectingRetries,
+  batchWithBisectingRetries,
+  flipArgs,
+  safeInvoke,
+  wrap,
+} from './function'
 
 describe('wrapping function', () => {
   test('wrapping an incrementing function with identity before and after should end up as +1', () => {
@@ -52,5 +58,147 @@ describe('safeInvoke', () => {
     }
     expect(safeInvoke(add, 1, 2).success).toBe(true)
     expect((safeInvoke(add, 1, 2) as any).result).toBe(3)
+  })
+})
+
+describe('batchWithBisectingRetries', () => {
+  test('should return results for arrays that do not cause errors', () => {
+    const exampleFn = (arr: number[]): number => arr.reduce((a, b) => a + b, 0)
+    const arr = [1, 2, 3, 4, 5, 6]
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([21])
+    expect(result.failures).toEqual([])
+  })
+
+  test('should handle errors by bisecting the array', () => {
+    const exampleFn = (arr: number[]): number[] => {
+      if (arr.includes(3)) {
+        throw new Error('Contains 3')
+      }
+      return arr
+    }
+    const arr = [1, 2, 3, 4, 5, 6]
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([[1], [2], [4, 5, 6]])
+    expect(result.failures.length).toBe(1)
+    expect((result.failures[0].error as Error).message).toBe('Contains 3')
+  })
+
+  test('should handle errors correctly with multiple error-prone elements', () => {
+    const exampleFn = (arr: number[]) => {
+      if (arr.includes(3) || arr.includes(5)) {
+        throw new Error('Contains 3 or 5')
+      }
+      return arr
+    }
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([[1, 2], [4], [6, 7, 8, 9, 10]])
+    expect(result.failures.length).toBe(2)
+    expect(result.failures.map((d) => d.item)).toEqual([3, 5])
+    expect((result.failures[0].error as Error).message).toBe('Contains 3 or 5')
+    expect((result.failures[1].error as Error).message).toBe('Contains 3 or 5')
+  })
+
+  test('should handle an empty array', () => {
+    const exampleFn = (arr: number[]): number => arr.reduce((a, b) => a + b, 0)
+    const arr: number[] = []
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([])
+    expect(result.failures).toEqual([])
+  })
+
+  test('should handle an array with one element that causes an error', () => {
+    const exampleFn = (arr: number[]): number => {
+      if (arr[0] === 1) {
+        throw new Error('Contains 1')
+      }
+      return arr[0]
+    }
+    const arr = [1]
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([])
+    expect(result.failures.length).toBe(1)
+    expect(result.failures[0].item).toBe(1)
+    expect((result.failures[0].error as Error).message).toBe('Contains 1')
+  })
+
+  test('should handle an array with one element that does not cause an error', () => {
+    const exampleFn = (arr: number[]): number => arr[0]
+    const arr = [1]
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([1])
+    expect(result.failures).toEqual([])
+  })
+})
+
+describe('asyncBatchWithBisectingRetries', () => {
+  test('should return results for arrays that do not cause errors', async () => {
+    const exampleFn = async (arr: number[]) => arr.reduce((a, b) => a + b, 0)
+    const arr = [1, 2, 3, 4, 5, 6]
+    const result = await asyncBatchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([21])
+    expect(result.failures).toEqual([])
+  })
+
+  test('should handle errors by bisecting the array', async () => {
+    const exampleFn = async (arr: number[]) => {
+      if (arr.includes(3)) {
+        throw new Error('Contains 3')
+      }
+      return arr
+    }
+    const arr = [1, 2, 3, 4, 5, 6]
+    const result = await asyncBatchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([[1], [2], [4, 5, 6]])
+    expect(result.failures.length).toBe(1)
+    expect((result.failures[0].error as Error).message).toBe('Contains 3')
+  })
+
+  test('should handle errors correctly with multiple error-prone elements', async () => {
+    const exampleFn = (arr: number[]) => {
+      if (arr.includes(3) || arr.includes(5)) {
+        throw new Error('Contains 3 or 5')
+      }
+      return arr
+    }
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const result = batchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([[1, 2], [4], [6, 7, 8, 9, 10]])
+    expect(result.failures.length).toBe(2)
+    expect(result.failures.map((d) => d.item)).toEqual([3, 5])
+    expect((result.failures[0].error as Error).message).toBe('Contains 3 or 5')
+    expect((result.failures[1].error as Error).message).toBe('Contains 3 or 5')
+  })
+
+  test('should handle an empty array', async () => {
+    const exampleFn = async (arr: number[]) => arr.reduce((a, b) => a + b, 0)
+    const arr: number[] = []
+    const result = await asyncBatchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([])
+    expect(result.failures).toEqual([])
+  })
+
+  test('should handle an array with one element that causes an error', async () => {
+    const exampleFn = async (arr: number[]) => {
+      if (arr[0] === 1) {
+        throw new Error('Contains 1')
+      }
+      return arr[0]
+    }
+    const arr = [1]
+    const result = await asyncBatchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([])
+    expect(result.failures.length).toBe(1)
+    expect(result.failures[0].item).toBe(1)
+    expect((result.failures[0].error as Error).message).toBe('Contains 1')
+  })
+
+  test('should handle an array with one element that does not cause an error', async () => {
+    const exampleFn = async (arr: number[]) => arr[0]
+    const arr = [1]
+    const result = await asyncBatchWithBisectingRetries(exampleFn, arr)
+    expect(result.results).toEqual([1])
+    expect(result.failures).toEqual([])
   })
 })
--- src/pure-immutable/function.ts
+++ src/pure-immutable/function.ts
@@ -46,3 +46,83 @@ export function wrap<T, U>(
 }
 
 // TODO: partial application would be nice
+
+type Result<Item, R> = {
+  results: R[]
+  failures: { error: unknown; item: Item }[]
+}
+
+/**
+ * Do a batch job on an array, or if the job fails, split the array
+ * and retry on each half, recursively. Accumulate the results, as well
+ * as the unprocessed items and the errors they triggered.
+ *
+ * This function is useful when there may be some items that are simply
+ * unprocessable.
+ */
+export function batchWithBisectingRetries<Item, R>(
+  fn: (arr: Item[]) => R,
+  arr: Item[],
+): Result<Item, R> {
+  const result: Result<Item, R> = {
+    results: [],
+    failures: [],
+  }
+
+  function helper(batch: Item[]) {
+    if (batch.length === 0) {
+      return
+    }
+
+    try {
+      result.results.push(fn(batch))
+    } catch (error) {
+      if (batch.length === 1) {
+        result.failures.push({ error, item: batch[0] })
+        return
+      }
+      const mid = Math.floor(batch.length / 2)
+      helper(batch.slice(0, mid))
+      helper(batch.slice(mid))
+    }
+  }
+
+  helper(arr)
+
+  return result
+}
+
+/**
+ * Asynchronous version of `batchWithBisectingRetries`
+ */
+export async function asyncBatchWithBisectingRetries<Item, R>(
+  fn: (arr: Item[]) => Promise<R>,
+  arr: Item[],
+): Promise<Result<Item, R>> {
+  const result: Result<Item, R> = {
+    results: [],
+    failures: [],
+  }
+
+  async function helper(batch: Item[]) {
+    if (batch.length === 0) {
+      return
+    }
+
+    try {
+      result.results.push(await fn(batch))
+    } catch (error) {
+      if (batch.length === 1) {
+        result.failures.push({ error, item: batch[0] })
+        return
+      }
+      const mid = Math.floor(batch.length / 2)
+      await helper(batch.slice(0, mid))
+      await helper(batch.slice(mid))
+    }
+  }
+
+  await helper(arr)
+
+  return result
+}
